95912461dab9f16ec5922f439379d9d730337af2
pravinpoudel <prvnpoudel4@gmail.com>
2023-02-01 03:00:21 -0700

    inital code start

---
+++ scripts/heatCalculator.js
@@ -0,0 +1,49 @@
+import { Clock } from "../node_modules/three/build/three.module.js";
+let specificHeatCapacity = 4180;
+let intialTankTem = 20;
+let collectorOutputTemp = 22;
+let ambientTemp = 16;
+let tankSurfaceArea = 25;
+let upriseRate = 0.03;
+let tankLossCofficient = 0.25;
+let waterMass = 100;
+let equillibruim = false;
+const clock = new Clock();
+
+function computeHeatFlow(flowRate, specificHeatCapacity, t2, t1) {
+  return flowRate * specificHeatCapacity * (t2 - t1);
+}
+
+function isEquillibruim() {
+  return intialTankTem == collectorOutputTemp;
+}
+
+function update(delta) {
+  let inputFromUpriser = computeHeatFlow(
+    upriseRate,
+    specificHeatCapacity,
+    collectorOutputTemp,
+    intialTankTem
+  );
+
+  let lossToEnv = computeHeatFlow(
+    tankSurfaceArea,
+    tankLossCofficient,
+    intialTankTem,
+    ambientTemp
+  );
+
+  let totalInternalEnergy = delta * (inputFromUpriser - lossToEnv);
+  intialTankTem += totalInternalEnergy / (waterMass * specificHeatCapacity);
+}
+
+function animate() {
+  const delta = clock.getDelta();
+  update(delta);
+  if (!isEquillibruim()) {
+    console.log(intialTankTem);
+    requestAnimationFrame(animate);
+  }
+}
+
+animate();
