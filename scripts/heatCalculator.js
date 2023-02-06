973b716b4f13148b636bdc171b4cc78605c6848a
pravinpoudel <prvnpoudel4@gmail.com>
2023-02-06 03:25:13 -0700

    headmap texture not working

--- scripts/heatCalculator.js
+++ scripts/heatCalculator.js
@@ -1,22 +1,14 @@
-import { Clock } from "../node_modules/three/build/three.module.js";
-
-let specificHeatCapacity = 4180;
 let intialTankTem = 20;
-let ambientTemp = 20;
-let tankSurfaceArea = 10;
 let upriseRate = 0.03;
-let tankLossCofficient = 300;
-let waterMass = 100;
-let equillibruim = false;
-const clock = new Clock();
 const collectorArea = 1.5;
 const collectorLossFactor = 13.14;
 const transAbsorbCofficient = 0.3;
-let heatRemovedFactor;
-let Irradiance = 354;
 const Cpf = 4180;
+let waterMass = 100;
+let hotMass = 0.1;
+let coldMass = waterMass;
 
-function collectorRemoveFactor(Mf, Ac, Ul) {
+function collectorRemoveFactor(upriseRate, Ac, Ul) {
   return (
     ((upriseRate * Cpf) / (Ac * Ul)) *
     (1 - Math.exp((-1 * Ac * Ul) / (upriseRate * Cpf)))
@@ -39,6 +31,12 @@ function isEquillibruim() {
   return intialTankTem == collectorOutputTemp;
 }
 
+function stratumUpdate(deltaTime) {
+  hotMass += upriseRate * deltaTime;
+  let addedHot = hotMass / waterMass;
+  return addedHot;
+}
+
 function collectoUsabelHeat(Ti, Ta, Ir) {
   let Qu =
     collectorArea *
@@ -47,49 +45,16 @@ function collectoUsabelHeat(Ti, Ta, Ir) {
   return Qu;
 }
 
-function update(delta) {
-  // collector heat ann output temperature
-  let Qu = collectoUsabelHeat(intialTankTem, ambientTemp, Irradiance);
-  let collectorOutputTemp = computeCollectorOutputTemp(Qu, intialTankTem);
-  //
-  let inputFromUpriser = computeHeatFlow(
-    upriseRate,
-    specificHeatCapacity,
-    collectorOutputTemp,
-    intialTankTem
-  );
+const heatRemovedFactor = collectorRemoveFactor(
+  upriseRate,
+  collectorArea,
+  collectorLossFactor
+);
 
-  let lossToEnv = computeHeatLoss(
-    tankSurfaceArea,
-    tankLossCofficient,
-    intialTankTem,
-    ambientTemp
-  );
-  console.log(lossToEnv);
-
-  let totalInternalEnergy = delta * (inputFromUpriser - lossToEnv);
-  intialTankTem += totalInternalEnergy / (waterMass * specificHeatCapacity);
-  console.log(intialTankTem);
-}
-
-// function animate() {
-//   const delta = clock.getDelta();
-//   update(delta);
-//   if (!isEquillibruim()) {
-//     console.log(intialTankTem);
-//     requestAnimationFrame(animate);
-//   }
-// }
-
-function main() {
-  heatRemovedFactor = collectorRemoveFactor(
-    upriseRate,
-    collectorArea,
-    collectorLossFactor
-  );
-  console.log(heatRemovedFactor);
-  update(3600);
-  // animate();
-}
-console.log("Hi");
-main();
+export {
+  collectoUsabelHeat,
+  computeCollectorOutputTemp,
+  computeHeatFlow,
+  computeHeatLoss,
+  stratumUpdate,
+};
--- scripts/renderer.js
+++ scripts/renderer.js
@@ -8,7 +8,13 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
 import { CanvasTexture } from "three";
-
+import {
+  collectoUsabelHeat,
+  computeCollectorOutputTemp,
+  computeHeatFlow,
+  computeHeatLoss,
+  stratumUpdate,
+} from "./heatCalculator";
 let camera, renderer;
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffe9c0);
@@ -20,6 +26,26 @@ let collectorPosition = new THREE.Vector3(-50, 10, 0);
 let tankPosition = new THREE.Vector3(50, 10, 0);
 let texture1, texture2;
 
+let specificHeatCapacity = 4180;
+let intialTankTem = 20;
+let ambientTemp = 20;
+let tankSurfaceArea = 10;
+let upriseRate = 0.03;
+let tankLossCofficient = 300;
+let waterMass = 100;
+let Irradiance = 354;
+let tankMaterial;
+let maxTemp = 50;
+let minTemperature = 0;
+let colorArray = [
+  new THREE.Color(0, 0, 1),
+  new THREE.Color(0, 1, 1),
+  new THREE.Color(0, 1, 0),
+  new THREE.Color(1, 1, 0),
+  new THREE.Color(1, 0, 0),
+];
+let done = false;
+
 function init() {
   scene.position.set(0, 0, 0); // it is default value but for sanity
   camera = new THREE.PerspectiveCamera(
@@ -28,13 +54,14 @@ function init() {
     1,
     10000
   );
-  camera.position.set(0, 200, 0);
+  camera.position.set(0, 300, 50);
   camera.lookAt(0, 0, 0);
   renderer = new THREE.WebGLRenderer();
   renderer.shadowMap.enabled = true;
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
   controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
 
   window.addEventListener("resize", onWindowResize, false);
   document.body.appendChild(stats.dom);
@@ -46,30 +73,103 @@ function init() {
   scene.add(gridHelper);
   texture1 = createPipe(80, 10, "right", -10, 5, 40);
   texture2 = createPipe(80, 10, "left", -10, 5, -40);
+  addCollector_Tank(collectorPosition, tankPosition);
 }
+const beforeCompileShader = (shader) => {
+  shader.uniforms.colors = tankMaterial.userData.uniforms.colors;
+  shader.uniforms.heatRatio = tankMaterial.userData.uniforms.heatRatio;
+  shader.fragmentShader = `
+  uniform vec3 colors[2];
+  uniform float heatRatio;
+  uniform sampler2D heatTexture;
+  uniform float hotTemp;
+  uniform float coldTemp;
+  ${shader.fragmentShader}
+`.replace(
+    `#include <color_fragment>`,
+    `#include <color_fragment>
+    vec3 coldColor = texture(heatTexture, vec2(5, 0.5)).rgb;
+    vec3 hotColor = texture(heatTexture, vec2(0.5, 0.5)).rgb;
+    float colorRatio = smoothstep( heatRatio-0.5, heatRatio + 0.5, vUv.y);
+    diffuseColor.rgb = mix(coldColor, hotColor, colorRatio);
+  `
+  );
+};
 
 function addCollector_Tank(collectorPosition, tankPosition) {
   const tankGeometry = new THREE.PlaneGeometry(50, 90);
-  const tankMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffff00,
+  tankMaterial = new THREE.MeshBasicMaterial({
+    onBeforeCompile: (shader) => {
+      beforeCompileShader(shader);
+    },
     side: THREE.DoubleSide,
   });
 
-  const collectorGeometry = new THREE.PlaneGeometry(20, 50);
-  const collectorMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffcc00,
-    side: THREE.DoubleSide,
-  });
+  tankMaterial.defines = { USE_UV: "" };
+  let heatMapTexture = new THREE.TextureLoader().load(
+    "./public/cool-warm-colormap.png",
+    function (texture) {
+      texture.needsUpdate = true;
+      texture.generateMipmaps = true;
+      tankMaterial.userData = {
+        uniforms: {
+          heatRatio: { value: 0.8 },
+          colors: {
+            value: [new THREE.Color(0, 0, 1), new THREE.Color(0, 1, 1)],
+          },
+          heatTexture: {
+            type: "t",
+            value: texture,
+          },
+          hotTemp: {
+            value: 0.7,
+          },
+          coldTemp: {
+            value: 0.1,
+          },
+        },
+      };
+      tankMaterial.userData.uniforms.needsUpdate = true;
+      let tank = new THREE.Mesh(tankGeometry, tankMaterial);
+      tank.rotation.x = Math.PI * 0.5;
+      const collectorGeometry = new THREE.PlaneGeometry(20, 50);
+      const collectorMaterial = new THREE.MeshBasicMaterial({
+        side: THREE.DoubleSide,
+      });
+      collectorMaterial.onBeforeCompile = (shader) => {
+        beforeCompileShader(shader);
+      };
+      collectorMaterial.defines = { USE_UV: "" };
+      collectorMaterial.userData = {
+        uniforms: {
+          heatRatio: { value: 0.5 },
+          heatTexture: {
+            type: "t",
+            value: texture,
+          },
+          colors: {
+            value: [new THREE.Color(1, 0, 0), new THREE.Color(1, 1, 1)],
+          },
+          hotTemp: {
+            value: 0.7,
+          },
+          coldTemp: {
+            value: 0.1,
+          },
+        },
+      };
 
-  let collector = new THREE.Mesh(collectorGeometry, collectorMaterial);
-  collector.rotation.x = Math.PI * 0.5;
-  let tank = new THREE.Mesh(tankGeometry, tankMaterial);
-  tank.rotation.x = Math.PI * 0.5;
-  scene.add(tank);
-  scene.add(collector);
+      collectorMaterial.needsUpdate = true;
+      let collector = new THREE.Mesh(collectorGeometry, collectorMaterial);
+      collector.rotation.x = Math.PI * 0.5;
+      scene.add(tank);
+      scene.add(collector);
 
-  tank.position.copy(tankPosition);
-  collector.position.copy(collectorPosition);
+      tank.position.copy(tankPosition);
+      collector.position.copy(collectorPosition);
+      done = true;
+    }
+  );
 }
 
 function createPipe(height, radius, direction, x, y, z) {
@@ -137,14 +237,42 @@ function onWindowResize() {
   render();
 }
 
-function animate() {
-  addCollector_Tank(collectorPosition, tankPosition);
+function heatCalculatorUpdate(deltaTime) {
+  let Qu = collectoUsabelHeat(intialTankTem, ambientTemp, Irradiance);
+  let collectorOutputTemp = computeCollectorOutputTemp(Qu, intialTankTem);
+  let inputFromUpriser = computeHeatFlow(
+    upriseRate,
+    specificHeatCapacity,
+    collectorOutputTemp,
+    intialTankTem
+  );
+
+  let lossToEnv = computeHeatLoss(
+    tankSurfaceArea,
+    tankLossCofficient,
+    intialTankTem,
+    ambientTemp
+  );
+  let totalInternalEnergy = deltaTime * (inputFromUpriser - lossToEnv);
+  intialTankTem += totalInternalEnergy / (waterMass * specificHeatCapacity);
+  // console.log(intialTankTem);
+}
+
+function update() {
   let deltaTime = clock.getDelta();
-  texture1.offset.x -= deltaTime;
-  texture2.offset.x -= deltaTime;
-  render();
-  stats.update();
-  requestAnimationFrame(animate);
+  deltaTime *= 100;
+  if (done) {
+    heatCalculatorUpdate(deltaTime);
+    tankMaterial.userData.uniforms.heatRatio.value = stratumUpdate(deltaTime);
+    tankMaterial.userData.uniforms.hotTemp = 30 / 40;
+    tankMaterial.userData.uniforms.coldTemp = 20 / 40;
+    // console.log(m.userData.uniforms.heatRatio);
+    texture1.offset.x -= deltaTime * 0.008;
+    texture2.offset.x -= deltaTime * 0.008;
+    render();
+    stats.update();
+  }
+  requestAnimationFrame(update);
 }
 
 function render() {
@@ -153,4 +281,5 @@ function render() {
 }
 
 init();
-animate();
+update();
+// console.clear();
